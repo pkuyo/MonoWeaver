b05d223e58b5dd98adc85d1b02536e99efe085b3
Pkuyo <miku_mi@foxmail.com>
2026-06-25 11:12:33 +0800

    修正mono cecil版本为[0.10.0-0.10.4]

---
+++ benchmarks/MonoWeaver.HookBenchmarks/IsExternalInit.cs
@@ -0,0 +1,9 @@
+﻿
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    // 仅供编译器识别，避免出现在 IntelliSense 中
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit;
+}
